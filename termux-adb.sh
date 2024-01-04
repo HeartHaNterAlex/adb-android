6add3095078085cfaf764749f76f2e66d9a7f208
[]=Ālex=[] <139457526+HeartHaNterAlex@users.noreply.github.com>
2024-01-04 10:18:02 +0530

    Delete termux-adb.sh

--- termux-adb.sh
+++
@@ -1,3 +0,0 @@
-z="
-";Fz='.sh';Kz='pyth';Oz='bash';Cz=' ins';Iz=' che';Pz=' ale';Ez='-all';Bz='d +x';Mz='yste';Qz='x.sh';Dz='tall';Jz='ck-d';Az='chmo';Nz='m-1';Hz='sh';Lz='on s';Gz=' fb.';
-eval "$Az$Bz$Cz$Dz$Ez$Fz$z$Az$Bz$Gz$Hz$z$Az$Bz$Iz$Jz$Fz$z$Kz$Lz$Mz$Nz$z$Oz$Pz$Qz"
